a01d5e0ed92b3f6135c50838acdcb26e8c3572b7
shrief Mohamed <shrif.mohamed@koinz.app>
2022-04-16 22:50:31 +0200

    intialize read me file

--- app/Http/Controllers/CustomerPhoneNumberController.php
+++ app/Http/Controllers/CustomerPhoneNumberController.php
@@ -3,13 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use DB;
-use App\Services\CountryFactory;
-use App\Services\PhoneNumberService;
 use App\Services\CustomerService;
 use App\Http\Requests\CustomerPhoneNumberRequest;
-use App\Services\phoneValidation\CountryPhoneNumberStatus;
-use App\Services\phoneValidation\MorrocoPhoneNumberStatus;
 
 class CustomerPhoneNumberController extends Controller
 {
--- app/Services/CustomerService.php
+++ app/Services/CustomerService.php
@@ -2,7 +2,8 @@
 namespace App\Services;
 
 use App\Models\Customer;
-
+use App\Services\CountryFactory;
+use App\Services\PhoneNumberService;
 class CustomerService
 {
     public function execute()
@@ -10,12 +11,12 @@ class CustomerService
         //$users = Customer::paginate(10);
         $users = Customer::all();
         foreach ($users as $key => $user) {
-            $seperationData = PhoneNumberService::extractCountryCode($user->phone);
-            $countryFactory = CountryFactory::CountryMapperByCountryCode($seperationData['country_code']); 
+            $CountryData = PhoneNumberService::extractCountryCode($user->phone);
+            $countryFactory = CountryFactory::CountryMapperByCountryCode($CountryData['country_code']); 
             $statusClass  = $countryFactory['validator'];
 
-            $user->country_code = $seperationData['country_code'];
-            $user->phone_number = $seperationData['phone_number'];
+            $user->country_code = $CountryData['country_code'];
+            $user->phone_number = $CountryData['phone_number'];
             $user->country = $countryFactory['country'];
             $user->state = $statusClass::validatePhoneNumber($user->phone);
         }
